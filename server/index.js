a964ad025a260763fe180f3fe08b38ca4027176c
andrewjohnso7 <andrewjohnso17@gmail.com>
2020-05-23 13:26:15 -0700

    updated server to host an array of strings under api/getStrings endpoint

--- server/index.js
+++ server/index.js
@@ -17,6 +17,12 @@ const app = express();
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 // app.use('/api', myApi);
 
+app.get('/api/getStrings', (req, res) => {
+  const strings = ['Aang', 'Katara', 'Toph', 'Sokka', 'Appa'];
+  res.send(JSON.stringify(strings));
+  console.log('Sent list of items');
+});
+
 // In production we need to pass these values in instead of relying on webpack
 setup(app, {
   outputPath: resolve(process.cwd(), 'build'),
