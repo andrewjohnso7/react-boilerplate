34356cd6b5df00d1769f92964c6020d96cb2c6bf
andrewjohnso7 <andrewjohnso17@gmail.com>
2020-05-25 14:34:18 -0700

    added all updateStrings action, addNewString saga, and watchers for saga

--- app/containers/StringDisplay/sagas.js
+++ app/containers/StringDisplay/sagas.js
@@ -1,14 +1,20 @@
-import { all, call, put, takeLatest } from 'redux-saga/effects';
-import { fetchStrings } from './actions';
-import { API_SUCCESS, API_FAILURE, FETCH_STRINGS } from './constants';
+import { all, call, put, takeLatest, takeEvery } from 'redux-saga/effects';
+import { fetchStrings, updateStrings } from './actions';
+import {
+  FETCH_STRINGS,
+  FETCH_SUCCESS,
+  FETCH_FAILURE,
+  UPDATING_STRINGS,
+  UPDATE_FAILED,
+  UPDATE_SUCCESS,
+} from './constants';
 
 export function* fetchAllStrings() {
   try {
-    console.log('to call the right things');
     const strings = yield call(fetchStrings);
-    yield put({ type: API_SUCCESS, data: strings });
+    yield put({ type: FETCH_SUCCESS, strings });
   } catch (error) {
-    yield put({ type: API_FAILURE, error });
+    yield put({ type: FETCH_FAILURE, error });
   }
 }
 
@@ -16,6 +22,19 @@ export function* watchFetchStrings() {
   yield takeLatest(FETCH_STRINGS, fetchAllStrings);
 }
 
+export function* addNewString(action) {
+  try {
+    const newString = yield updateStrings(action.payload);
+    yield put({ type: UPDATE_SUCCESS, newString });
+  } catch (error) {
+    yield put({ type: UPDATE_FAILED });
+  }
+}
+
+export function* watchUpdatingStrings() {
+  yield takeEvery(UPDATING_STRINGS, addNewString);
+}
+
 export function* rootSaga() {
-  yield all([watchFetchStrings()]);
+  yield all([watchFetchStrings(), watchUpdatingStrings()]);
 }
