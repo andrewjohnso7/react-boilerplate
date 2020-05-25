6557b652fd05077f0e38790a073b629bc5783666
andrewjohnso7 <andrewjohnso17@gmail.com>
2020-05-24 20:34:12 -0700

    created new reducer for FETCH_STRINGS action creator

--- app/reducers.js
+++ app/reducers.js
@@ -8,11 +8,14 @@ import { connectRouter } from 'connected-react-router';
 import history from 'utils/history';
 import languageProviderReducer from 'containers/LanguageProvider/reducer';
 
+import fetchStringsReducer from './containers/StringDisplay/reducer';
+
 /**
  * Merges the main reducer with the router state and dynamically injected reducers
  */
 export default function createReducer(injectedReducers = {}) {
   const rootReducer = combineReducers({
+    strings: fetchStringsReducer,
     language: languageProviderReducer,
     router: connectRouter(history),
     ...injectedReducers,
