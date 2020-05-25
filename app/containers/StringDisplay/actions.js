8b17c4b27a8bcd720dfec34cdacb3889156efcde
andrewjohnso7 <andrewjohnso17@gmail.com>
2020-05-25 14:44:03 -0700

    fetch strings now returns a promise and added new updateStrings action

--- app/containers/StringDisplay/actions.js
+++ app/containers/StringDisplay/actions.js
@@ -1,5 +1,15 @@
 export const fetchStrings = () =>
   fetch(`api/getStrings`)
     .then(res => res.json())
-    .then(data => console.log('give this to the reducer', data))
     .catch(error => console.log('Something Broke ', error));
+
+export const updateStrings = post =>
+  fetch(`/api/addString`, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify(post),
+  })
+    .then(res => res.json())
+    .catch(error => console.log('We Broke it again: ', error));
