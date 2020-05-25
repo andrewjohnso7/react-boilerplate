6896aa3899b7ed3aabe6d0dc338947f479d1c8b6
andrewjohnso7 <andrewjohnso17@gmail.com>
2020-05-24 21:43:40 -0700

    reworked Sagas to yield actions

--- app/containers/StringDisplay/sagas.js
+++ app/containers/StringDisplay/sagas.js
@@ -1,23 +1,21 @@
-import { call, put, takeEvery, takeLatest } from 'redux-saga/effects';
+import { all, call, put, takeLatest } from 'redux-saga/effects';
 import { fetchStrings } from './actions';
 import { API_SUCCESS, API_FAILURE, FETCH_STRINGS } from './constants';
 
-export function* fetchAllStrings(action) {
+export function* fetchAllStrings() {
   try {
     console.log('to call the right things');
-    const strings = yield call(fetchStrings, action.payload);
-    yield put({ type: API_SUCCESS, strings });
+    const strings = yield call(fetchStrings);
+    yield put({ type: API_SUCCESS, data: strings });
   } catch (error) {
-    console.log('to call the wrong things');
     yield put({ type: API_FAILURE, error });
   }
 }
 
 export function* watchFetchStrings() {
-  console.log('Trying');
-  yield takeEvery(FETCH_STRINGS, fetchAllStrings);
+  yield takeLatest(FETCH_STRINGS, fetchAllStrings);
 }
 
-export function* mySaga() {
-  console.log('Hello From my saga');
+export function* rootSaga() {
+  yield all([watchFetchStrings()]);
 }
