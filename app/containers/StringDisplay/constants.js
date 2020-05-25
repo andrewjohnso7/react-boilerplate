bd4ef93fc2cda9276deecdd6e90cd8f73ccdfbe6
andrewjohnso7 <andrewjohnso17@gmail.com>
2020-05-25 14:39:58 -0700

    renamed constants for clarity and added new cases for api success and failures

--- app/containers/StringDisplay/constants.js
+++ app/containers/StringDisplay/constants.js
@@ -1,3 +1,6 @@
 export const FETCH_STRINGS = 'FETCH_STRINGS';
-export const API_SUCCESS = 'API_SUCCESS';
-export const API_FAILURE = 'API_FAILURE';
+export const FETCH_SUCCESS = 'FETCH_SUCCESS';
+export const FETCH_FAILURE = 'FETCH_FAILURE';
+export const UPDATING_STRINGS = 'UPDATING_STRINGS';
+export const UPDATE_SUCCESS = 'UPDATE_SUCCESS';
+export const UPDATE_FAILED = 'UPDATE_FAILED';
