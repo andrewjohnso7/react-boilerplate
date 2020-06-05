5e68f473e00d98b3502080211fe0c01518eb96cf
andrewjohnso7 <andrewjohnso17@gmail.com>
2020-06-05 12:00:50 -0700

    added component handling of errors reported from redux

--- app/containers/StringForm/index.js
+++ app/containers/StringForm/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
-import { UPDATING_STRINGS } from '../StringDisplay/constants';
+import { addString } from '../StringDisplay/actionCreators';
 import {
   MainContainer,
   Section,
@@ -58,7 +58,15 @@ export class StringForm extends React.Component {
           <h1>Add a Name</h1>
           <form onSubmit={this.onSubmit}>
             <FormContainer>
-              <label htmlFor="New String">Enter name here: </label>
+              <label htmlFor="New String">
+                <br />
+                {added ? (
+                  <div>{added.body} successfully added to the list</div>
+                ) : (
+                  <div>Please attempt to enter a string</div>
+                )}
+                <br />{' '}
+              </label>
               <br />
               <TextArea
                 placeholder={this.state.body || "What's your name?"}
@@ -70,12 +78,6 @@ export class StringForm extends React.Component {
               <SubmitString type="submit">Add name</SubmitString>
             </FormContainer>
           </form>
-          <br />
-          {added ? (
-            <div>{added.body} successfully added to the list</div>
-          ) : (
-            <div>Sorry, please attempt to enter a string again</div>
-          )}
           <br />
           <StringNavigation to="/">See list of names</StringNavigation>
         </Section>
@@ -90,10 +92,7 @@ const mapStateToProps = state => ({
 });
 
 export const mapDispatchToProps = {
-  addString: payload => ({
-    type: UPDATING_STRINGS,
-    payload,
-  }),
+  addString,
 };
 
 export default connect(
